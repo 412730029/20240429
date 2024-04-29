b7cd2001b59f03c9175cd1e484aa5e332bd7dd61
412730029 <150638405+412730029@users.noreply.github.com>
2024-04-29 10:32:15 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
+
+var captureGraphics
+var capture_width = 640
+var capture_height = 480
+var span = 10
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO)             //啟動攝影機
-  capture.size(320,240);                     //設定顯示畫面大小
+  capture = createCapture(VIDEO)                //啟動攝影機
+  capture.size(capture_width,capture_height);   //設定顯示畫面大小
+  captureGraphics = createGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
+  captureGraphics.scale(-1,1)
+  capture.hide()
+  
 }
 
 function draw() {
   background(220);
+  noStroke()
+
 push()
-  translate(capture,width/2-160, height/2-120)
-  image(capture,0,0)
+
+  translate(width/2-capture_width/2, height/2-capture_height/2)
+  captureGraphics.image(capture,0,0)
+  for(var x = 0;x<captureGraphics.width;x=x+span){
+    for(var y = 0;y<captureGraphics.height ; y=y+span){
+      var pixel = captureGraphics.get(x,y)
+      fill(pixel)
+      rect(x,y,10)
+    }
+  }
+
 pop()
+
 }
