2960bbd45d678b5bfa2757c9b026e2f925af642a
412730029 <150638405+412730029@users.noreply.github.com>
2024-04-29 11:46:24 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ function setup() {
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
   capture.hide()
+  
 
   //選鈕的介面
   radioElement = createRadio();
@@ -19,6 +20,8 @@ function setup() {
   radioElement.option("方塊")
   radioElement.option("圓圈")
   radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
   radioElement.style("font-size","30px")
   
@@ -48,6 +51,25 @@ push()
       fill(bk)
       ellipse(x,y,span)                            //span代表圓圈的直徑
       ellipse(x,y,span*map(bk,0,255,0,1))          //透過map指令，bk在0~255之間，依照比率設定0~1間的數字
+     }
+     if(radioElement.value()=="紅底"){
+      colorMode(HSB)
+      fill(pixel[0],80,80)
+      rectMode(CENTER)
+      rect(x,y,span*0.8)
+      // ellipse(x,y,span)                          //span代表圓圈的直徑
+      fill(0)
+      ellipse(x,y,10)         
+     }
+     if(radioElement.value()=="文字"){
+      const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+      let txt = "一二三四五田雷電龕龘"
+      bk = (pixel[0]+pixel[1]+pixel[2])/3
+      let bkId = int(map(bk, 0, 255, 9, 0))
+      textSize(span)
+      textStyle(BOLD)
+      text(txt[bkId],x,y)
+
      }
   }
 }
